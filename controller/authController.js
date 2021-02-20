8e34ce150a945af1590e4f8aa20dc129dff6de8a
Sushant <sushantberiwal@gmail.com>
2021-02-20 21:02:40 +0530

    added some checks

--- controller/authController.js
+++ controller/authController.js
@@ -1,11 +1,15 @@
 function checkAuth(req , res){
     if(req.user){
         // user is logged in
+        console.log("Inside if req.user exist");
+        console.log(req.user);
         res.json({
             isAuth: true ,
             user : req.user
         })
     }else{
+        console.log("Inside if req.user dont exist");
+        console.log(req.user);
         res.json({
             isAuth:false
         })
