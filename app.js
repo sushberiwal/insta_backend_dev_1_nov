7f0dba472cfd57b5b684a94c6cf4f306cac50c18
Sushant <sushantberiwal@gmail.com>
2021-02-21 10:24:26 +0530

    removed samesite none

--- app.js
+++ app.js
@@ -15,8 +15,7 @@ app.use(express.static("public"));
 app.use(cookie({
     maxAge: 30 * 24 * 60 * 60 * 1000,
     keys: ["askjdbajsbvfwfbha"],
-    httpOnly:false,
-    sameSite:"none",
+    httpOnly:false
 }))
 
 app.use(passport.initialize());
